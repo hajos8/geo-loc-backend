69eb99715b128c73d946d2f10553b058ebd24c97
hajos8 <rabb.andras@gmail.com>
2026-01-21 08:07:56 +0100

    post

--- src/index.cjs
+++ src/index.cjs
@@ -10,6 +10,13 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.post('/geolocation', (req, res) => {
+    const { latitude, longitude } = req.body
+    console.log({ latitude, longitude })
+
+    return res.status(201).json('ok')
+})
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
